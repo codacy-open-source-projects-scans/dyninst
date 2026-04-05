b9c5a45a4f8a35acd5c5a188a02305e5ee431587
Codacy Archive <codacy-archive@local>
2026-04-05 00:20:54 +0000

    snapshot 2026-04-05T00:20:54.796043+00:00

--- tests/opcode_tests.cpp
+++ tests/opcode_tests.cpp
@@ -11,19 +11,20 @@ bool verify(Instruction const &insn, opcode_test const &expected) {
   entryID actual_opcode = insn.getOperation().getID();
   entryID actual_encoded_opcode = insn.getEncodedOperation().getID();
   entryID expected_opcode = expected.opcode;
-  entryID expected_encoded_opcode = expected.encoded_opcode;
+  boost::optional<entryID> expected_encoded_opcode = expected.encoded_opcode;
 
   bool failed = false;
 
   if (actual_opcode != expected_opcode) {
     std::cerr << "Mismatched opcode\n";
     std::cerr << "Expected: " << expected.opcode_mnemonic
+              << " (" << static_cast<int>(insn.getOperation().getID()) << ") "
               << ", Found: " << insn.getOperation().format();
     std::cerr << '\n';
     failed = true;
   }
 
-  if (actual_encoded_opcode != expected_encoded_opcode) {
+  if (expected_encoded_opcode && actual_encoded_opcode != *expected_encoded_opcode) {
     std::cerr << "Mismatched encoded opcode\n";
     std::cerr << "Expected: " << expected.encoded_opcode_mnemonic
               << ", Found: " << insn.getEncodedOperation().format();
